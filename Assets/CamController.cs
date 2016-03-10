09bad4a57fdc5b6a9e1dc523f3c66195e6e075a0
BeardMage <mesk.utp@gmail.com>
2016-03-10 08:47:54 +0100

    Uploaded cam controller
    Tony c'est toi qui bosses en 5.3.1 ?

---
+++ Assets/CamController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour
+{
+	public float speed;
+	public float rotationSpeed;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		if(h != 0)
+		{
+			transform.Rotate(Vector3.up, rotationSpeed * h * Time.deltaTime);
+		}
+
+		if(v !=0)
+		{
+			transform.Translate(transform.forward * speed * Time.deltaTime * v, Space.World);
+		}
+	}
+}
